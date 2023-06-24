331ef442f1beab86e1a8e1d8a556c7f8432c8daf
gene courtney <genecourtney7@gmail.com>
2023-06-23 23:47:53 -0500

    should show dropdown responce. updated html code to java as suggested.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-if(true){
-    
+function result(){
+    const javascript;
+    if(document.getElementsById("1_yes,2_yes,3_yes,4_no,5_yes").checked)
+    const cSharp;
+    if(document.getElementsById("1_yes,2_yes,3_yes,4_no,5_yes").checked)
+    const Python;
+    if(document.getElementsById("1_yes,2_yes,3_yes,4_no,5_yes").checked)
+   
 }
