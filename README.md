ed3be218a3ecbcf1feb58fbc2a78890eb553131c
gene courtney <genecourtney7@gmail.com>
2023-07-11 22:54:43 -0500

    fixed formating issues as well as fixing the readMe.md file.

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
 # ProgramQuiz
-#### By _**Mark Courtney**_
-#### _This is my fisrt from scratch interative html page that gets input from the user._
+#### By: _**Mark Courtney**_
+#### _This is my first from scratch interactive html page that gets input from the user._
 https://genecourtney7.github.io/ProgramQuiz/
 ## Technologies used
-*_developer.mozilla.org_
-*_Youtube.com_
-*_learnhowtocode.com lessons_
-*_google.com_
-*_vs code terminal_
+* _developer.mozilla.org_
+* _Youtube.com_
+* _learnhowtocode.com lessons_
+* _google.com_
+* _vs code terminal_
 ## Description
-*Small survey to help determine what code language to learn.
+* Small survey to help determine what code language to learn.
 ## Setup/Installation Requirements
-*Clone this repository to your desktop.
-*click the url and open with compatible browser. 
-*click "Start Quiz"
+* Clone this repository to your desktop: `$ git clone https://github.com/genecourtney7/ProgramQuiz.git`
+* Open `index.html` in a web browser. 
 ## Known Bugs
 * No known Bugs
 ## licence 
-*copyright 6-25-23 author: Mark Courtney
+* MIT Copyright (c) _6-25-23_ Author: _Mark Courtney_
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,69 @@
-//function handleSelect(event) {
-  //  event.preventDefault();
- //   const selection = document.getElementById("q1").value;
- // }
-  
- // window.addEventListener("load", function() {
-  //  document.getElementById("select-form").addEventListener("submit", handleSelect);
- // });
+window.addEventListener("load",function(){
+    const form = document.getElementById("questions");
 
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        getValue();
+    })
 
-
-
- function getValue(){
+    function getValue(){
     
-    let input1 = document.getElementById("q1");
-    let input2 = document.getElementById("q2");
-    let input3 = document.getElementById("q3");
-    let input4 = document.getElementById("q4");
-    let input5 = document.getElementById("q5");
-    let result = document.getElementById("result");
-
-    let userInput1 = document.getElementById("q1").value;
-    let str1 = userInput1.toString();
-
-    let userInput2 = document.getElementById("q2").value;
-    let str2 = userInput2.toString();
-
-    let userInput3 = document.getElementById("q3").value;
-    let str3 = userInput3.toString();
-
-    let userInput4 = document.getElementById("q4").value;
-    let str4 = userInput4.toString();
-
-    let userInput5 = document.getElementById("q5").value;
-    let str5 = userInput5.toString();
-
-
-    
-if (str1 == "no"){
-    result.innerHTML = ("Reconsider Learning to Code.")
-} else if (str1+str2+str3+str4+str5 == "yesyesyesyesyes") {
-    result.innerHTML = ("C#")
-} else if (str1+str2+str3+str4+str5 == "yesyesyesyesno") {
-    result.innerHTML = ("JavaScript")
-} else if (str1+str2+str3+str4+str5 == "yesyesyesnoyes") {
-    result.innerHTML = ("Python")
-} else if (str1+str2+str3+str4+str5 == "yesyesnoyesyes") {
-    result.innerHTML = ("Python")
-} else if (str1+str2+str3+str4+str5 == "yesnoyesyesyes") {
-    result.innerHTML = ("C#")
-} else if (str1+str2+str3+str4+str5 == "yesyesyesnono") {
-    result.innerHTML = ("JavaScript")
-} else if (str1+str2+str3+str4+str5 == "yesyesnoyesno") {
-    result.innerHTML = ("Python")
-} else if (str1+str2+str3+str4+str5 == "yesnoyesyesno") {
-    result.innerHTML = ("JavaScript")
-} else if (str1+str2+str3+str4+str5 == "yesnoyesnoyes") {
-    result.innerHTML = ("JavaScript")
-} else if (str1+str2+str3+str4+str5 == "yesnonoyesyes") {
-    result.innerHTML = ("Python")
-} else if (str1+str2+str3+str4+str5 == "yesnonoyesno") {
-    result.innerHTML = ("C#")
-} else if (str1+str2+str3+str4+str5 == "yesnononoyes") {
-    result.innerHTML = ("C#")
-} else if (str1+str2+str3+str4+str5 == "yesnononono") {
-    result.innerHTML = ("Find out what you would like to program first.")
-}
-}
+      let input1 = document.getElementById("q1");
+      let input2 = document.getElementById("q2");
+      let input3 = document.getElementById("q3");
+      let input4 = document.getElementById("q4");
+      let input5 = document.getElementById("q5");
+      let result = document.getElementById("result");
+        
+      let userInput1 = document.getElementById("q1").value;
+      let str1 = userInput1.toString();
+        
+      let userInput2 = document.getElementById("q2").value;
+      let str2 = userInput2.toString();
+        
+      let userInput3 = document.getElementById("q3").value;
+      let str3 = userInput3.toString();
+        
+      let userInput4 = document.getElementById("q4").value;
+      let str4 = userInput4.toString();
+        
+      let userInput5 = document.getElementById("q5").value;
+      let str5 = userInput5.toString();
+        
+        
+      
+      if (str1 == "no"){
+          result.innerHTML = ("Reconsider Learning to Code.")
+      } else if (str1+str2+str3+str4+str5 == "yesyesyesyesyes") {
+          result.innerHTML = ("C#")
+      } else if (str1+str2+str3+str4+str5 == "yesyesyesyesno") {
+          result.innerHTML = ("JavaScript")
+      } else if (str1+str2+str3+str4+str5 == "yesyesyesnoyes") {
+          result.innerHTML = ("Python")
+      } else if (str1+str2+str3+str4+str5 == "yesyesnoyesyes") {
+          result.innerHTML = ("Python")
+      } else if (str1+str2+str3+str4+str5 == "yesnoyesyesyes") {
+          result.innerHTML = ("C#")
+      } else if (str1+str2+str3+str4+str5 == "yesyesyesnono") {
+          result.innerHTML = ("JavaScript")
+      } else if (str1+str2+str3+str4+str5 == "yesyesnoyesno") {
+          result.innerHTML = ("Python")
+      } else if (str1+str2+str3+str4+str5 == "yesnoyesyesno") {
+          result.innerHTML = ("JavaScript")
+      } else if (str1+str2+str3+str4+str5 == "yesnoyesnoyes") {
+          result.innerHTML = ("JavaScript")
+      } else if (str1+str2+str3+str4+str5 == "yesnonoyesyes") {
+          result.innerHTML = ("Python")
+      } else if (str1+str2+str3+str4+str5 == "yesnonoyesno") {
+          result.innerHTML = ("C#")
+      } else if (str1+str2+str3+str4+str5 == "yesnononoyes") {
+          result.innerHTML = ("C#")
+      } else if (str1+str2+str3+str4+str5 == "yesnononono") {
+          result.innerHTML = ("Find out what you would like to program first.")
+      }
+    }
+})
  
+
 
  
